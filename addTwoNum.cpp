ff7c913e01616bbc66a4295e98d9f3253e805a37
himanshi729 <pawarhimanshi913@gmail.com>
2026-05-11 22:50:54 +0530

    Added sum problem solution

---
+++ addTwoNum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+// class Solution{
+//      int a, b;
+//     public:
+//        int AddTwoNum(int a , int b){
+//         return a+b;
+//        }
+// };
+int add(int a, int b){
+    return a+b ;
+}
+
+int main(){
+    // Solution sol;
+    int x, y;
+
+    cin >> x >>  y ;
+
+    cout<<add(x, y);
+
+    return 0;
+    
+}
